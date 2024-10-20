21039eb70d73c7bc986b9c4f78002017db2ef038
yyboo <xxx@163.com>
2024-10-20 15:38:44 +0800

    新增数据库连接工具

---
+++ dbUtils/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Config struct {
+	User   string
+	Passwd string
+	Host   string
+	Port   int
+	DBName string
+}
+
+// NewDB returns a new database pool. only supports mysql
+func NewDB(config *Config) (dbPool *sql.DB, err error) {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true", config.User, config.Passwd, config.Host, config.Port, config.DBName)
+
+	if dbPool, err = sql.Open("mysql", dsn); err != nil {
+		return nil, fmt.Errorf("NewDB(): failed to open database, error: %w", err)
+	}
+
+	if err = dbPool.Ping(); err != nil {
+		return nil, fmt.Errorf("NewDB(): failed to ping database, error: %w", err)
+	}
+
+	return dbPool, nil
+}
